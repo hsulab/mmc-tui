731928f1b6bbb46aaa35f80d472e169a219b7628
jxu <ahcigar@foxmail.com>
2025-11-15 15:29:37 -0500

    feat: add palette

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,30 @@ import {
 } from "@opentui/core";
 
 import { setupKeybinds } from "./keybinds.ts";
+import { LattePalette } from "./palette.ts";
+
+/**
+ * Main TUI rendering function
+ */
+export function createLayoutElements(renderer: CliRenderer): void {
+  renderer.setBackgroundColor(LattePalette.base);
+}
 
 /**
  * Add components to the renderer
  */
 export function run(renderer: CliRenderer) {
+  createLayoutElements(renderer);
   renderer.root.add(
     Box(
       { alignItems: "center", justifyContent: "center", flexGrow: 1 },
       Box(
         { justifyContent: "center", alignItems: "flex-end" },
-        ASCIIFont({ font: "tiny", text: "MMC-TUI" }),
+        ASCIIFont({ font: "tiny", text: "MMC-TUI", color: LattePalette.text }),
         Text({
           content: "What will you build?",
           attributes: TextAttributes.DIM,
+          fg: LattePalette.text,
         }),
       ),
     ),
---
+++ src/palette.ts
@@ -0,0 +1,30 @@
+class LattePalette {
+  static flamingo = "#dd7878";
+  static pink = "#ea76cb";
+  static mauve = "#8839ef";
+  static red = "#d20f39";
+  static maroon = "#e64553";
+  static peach = "#fe640b";
+  static yellow = "#df8e1d";
+  static green = "#40a02b";
+  static teal = "#179299";
+  static rosewater = "#dc8a78";
+  static sky = "#04a5e5";
+  static sapphire = "#209fb5";
+  static blue = "#1e66f5";
+  static lavender = "#7287fd";
+  static text = "#4c4f69";
+  static subtext1 = "#5c5f77";
+  static subtext0 = "#6c6f85";
+  static overlay2 = "#7c7f93";
+  static overlay1 = "#8c8fa1";
+  static overlay0 = "#9ca0b0";
+  static surface2 = "#acb0be";
+  static surface1 = "#bcc0cc";
+  static surface0 = "#ccd0da";
+  static base = "#eff1f5";
+  static mantle = "#e6e9ef";
+  static crust = "#dce0e8";
+}
+
+export { LattePalette };
