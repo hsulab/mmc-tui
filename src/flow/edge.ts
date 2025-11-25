69396e9e31298b628f9a36817404fe201d4509c3
jxu <ahcigar@foxmail.com>
2025-11-25 15:49:45 -0500

    refact: move edge to graph

---
+++ src/flow/edge.ts
@@ -0,0 +1,95 @@
+import {
+  RGBA,
+  CliRenderer,
+  BoxRenderable,
+  FrameBufferRenderable,
+  OptimizedBuffer,
+} from "@opentui/core";
+
+import { LattePalette } from "../palette.ts";
+
+export type NodeEdge = { from: BoxRenderable; to: BoxRenderable };
+
+export class EdgeFrameBuffer extends FrameBufferRenderable {
+  private readonly BACKGROUND_COLOR: RGBA;
+  private readonly LINE_COLOR = RGBA.fromHex(LattePalette.subtext0);
+
+  private linemarker: string;
+
+  constructor(
+    renderer: CliRenderer,
+    id: string,
+    private readonly edgesProvider: () => NodeEdge[],
+    linemarker: string = "⋯", // "⠒"
+    backgroundColor: RGBA = RGBA.fromHex(LattePalette.base),
+  ) {
+    super(renderer, {
+      id,
+      width: renderer.terminalWidth,
+      height: renderer.terminalHeight,
+      zIndex: 90,
+    });
+
+    this.linemarker = linemarker;
+
+    this.BACKGROUND_COLOR = backgroundColor;
+  }
+
+  protected override renderSelf(buffer: OptimizedBuffer): void {
+    this.frameBuffer.clear(this.BACKGROUND_COLOR);
+
+    for (const edge of this.edgesProvider()) {
+      this.drawDottedLine(
+        Math.round(edge.from.x + edge.from.width / 2) - this.x,
+        Math.round(edge.from.y + edge.from.height / 2) - this.y,
+        Math.round(edge.to.x + edge.to.width / 2) - this.x,
+        Math.round(edge.to.y + edge.to.height / 2) - this.y,
+      );
+    }
+
+    super.renderSelf(buffer);
+  }
+
+  private drawDottedLine(x0: number, y0: number, x1: number, y1: number) {
+    let currentX = x0;
+    let currentY = y0;
+
+    const dx = Math.abs(x1 - x0);
+    const sx = x0 < x1 ? 1 : -1;
+    const dy = -Math.abs(y1 - y0);
+    const sy = y0 < y1 ? 1 : -1;
+    let err = dx + dy;
+    let drawDot = true;
+
+    while (true) {
+      if (
+        drawDot &&
+        currentX >= 0 &&
+        currentY >= 0 &&
+        currentX < this.width &&
+        currentY < this.height
+      ) {
+        this.frameBuffer.setCell(
+          currentX,
+          currentY,
+          this.linemarker,
+          this.LINE_COLOR,
+          this.BACKGROUND_COLOR,
+        );
+      }
+
+      if (currentX === x1 && currentY === y1) break;
+      const e2 = 2 * err;
+      if (e2 >= dy) {
+        err += dy;
+        currentX += sx;
+      }
+      if (e2 <= dx) {
+        err += dx;
+        currentY += sy;
+      }
+
+      drawDot = !drawDot;
+    }
+  }
+}
--- src/window/flow.ts
+++ src/window/flow.ts
@@ -6,8 +6,6 @@ import {
   RGBA,
   CliRenderer,
   BoxRenderable,
-  FrameBufferRenderable,
-  OptimizedBuffer,
   SelectRenderable,
   SelectRenderableEvents,
   type SelectOption,
@@ -16,92 +14,7 @@ import {
 import { Pane, type Rect } from "./base.ts";
 import { LattePalette } from "../palette.ts";
 import { DraggableBox, type SelectableBoxRenderable } from "../flow/graph.ts";
-
-type NodeEdge = { from: BoxRenderable; to: BoxRenderable };
-
-class EdgeFrameBuffer extends FrameBufferRenderable {
-  private readonly BACKGROUND_COLOR: RGBA;
-  private readonly LINE_COLOR = RGBA.fromHex(LattePalette.subtext0);
-
-  private linemarker: string;
-
-  constructor(
-    renderer: CliRenderer,
-    id: string,
-    private readonly edgesProvider: () => NodeEdge[],
-    backgroundColor: RGBA,
-    linemarker: string = "⋯", // "⠒"
-  ) {
-    super(renderer, {
-      id,
-      width: renderer.terminalWidth,
-      height: renderer.terminalHeight,
-      zIndex: 90,
-    });
-
-    this.linemarker = linemarker;
-
-    this.BACKGROUND_COLOR = backgroundColor;
-  }
-
-  protected override renderSelf(buffer: OptimizedBuffer): void {
-    this.frameBuffer.clear(this.BACKGROUND_COLOR);
-
-    for (const edge of this.edgesProvider()) {
-      this.drawDottedLine(
-        Math.round(edge.from.x + edge.from.width / 2) - this.x,
-        Math.round(edge.from.y + edge.from.height / 2) - this.y,
-        Math.round(edge.to.x + edge.to.width / 2) - this.x,
-        Math.round(edge.to.y + edge.to.height / 2) - this.y,
-      );
-    }
-
-    super.renderSelf(buffer);
-  }
-
-  private drawDottedLine(x0: number, y0: number, x1: number, y1: number) {
-    let currentX = x0;
-    let currentY = y0;
-
-    const dx = Math.abs(x1 - x0);
-    const sx = x0 < x1 ? 1 : -1;
-    const dy = -Math.abs(y1 - y0);
-    const sy = y0 < y1 ? 1 : -1;
-    let err = dx + dy;
-    let drawDot = true;
-
-    while (true) {
-      if (
-        drawDot &&
-        currentX >= 0 &&
-        currentY >= 0 &&
-        currentX < this.width &&
-        currentY < this.height
-      ) {
-        this.frameBuffer.setCell(
-          currentX,
-          currentY,
-          this.linemarker,
-          this.LINE_COLOR,
-          this.BACKGROUND_COLOR,
-        );
-      }
-
-      if (currentX === x1 && currentY === y1) break;
-      const e2 = 2 * err;
-      if (e2 >= dy) {
-        err += dy;
-        currentX += sx;
-      }
-      if (e2 <= dx) {
-        err += dx;
-        currentY += sy;
-      }
-
-      drawDot = !drawDot;
-    }
-  }
-}
+import { EdgeFrameBuffer, type NodeEdge } from "../flow/edge.ts";
 
 export class FlowPane extends Pane {
   private keybinds: ((key: any) => void) | null = null;
@@ -173,6 +86,7 @@ export class FlowPane extends Pane {
       this.renderer,
       `${this.id}-edges`,
       () => this.edges,
+      "⋯",
       RGBA.fromHex(LattePalette.surface0),
     );
     this.edgeLayer.top = 0;
